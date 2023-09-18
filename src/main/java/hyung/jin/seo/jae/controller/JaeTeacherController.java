db65f9db24780be39cb3e337c2e6c384ae28d72c
Jin <jin@macbookk2023.com>
2023-09-19 01:13:03 +1000

    Teacher, updateMemo done

--- src/main/java/hyung/jin/seo/jae/controller/JaeTeacherController.java
+++ src/main/java/hyung/jin/seo/jae/controller/JaeTeacherController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -127,5 +128,18 @@ public class JaeTeacherController {
 		}
 		model.addAttribute(JaeConstants.TEACHER_LIST, dtos);
 		return "teacherListPage";
+	}
+
+	// update memo by Id
+	@PostMapping("/updateMemo/{id}")
+	@ResponseBody
+	public ResponseEntity<String> updateMemo(@PathVariable("id") Long id, @RequestBody(required = false) String info) {
+		try{
+			teacherService.updateTeacherMemo(id, info);
+			return ResponseEntity.ok("Memo Update Success");
+		}catch(Exception e){
+			System.out.println("No teacher found");
+			return ResponseEntity.ok("Material Update Failed");
+		}
 	}
 }
