e8c541796665a5916e0594304021c0f576d27b8b
Jin <jin@macbookk2023.com>
2023-10-02 00:20:49 +1100

    search criteria info 추가 atttendance

---
+++ src/main/java/hyung/jin/seo/jae/dto/SearchCriteriaDTO.java
@@ -0,0 +1,28 @@
+package hyung.jin.seo.jae.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class SearchCriteriaDTO {
+    
+	private String id;
+
+	private String state;
+
+	private String branch;
+
+	private String grade;
+
+	private String fromDate;
+
+	private String toDate;
+
+	private String extra;
+
+}
