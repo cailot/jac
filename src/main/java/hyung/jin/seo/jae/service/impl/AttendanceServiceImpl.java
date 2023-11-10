c239439c1e43f49c654a7e9ed16eba2c6b8efd6f
Jin <jin@macbookk2023.com>
2023-11-11 00:30:41 +1100

    add current week for student number

--- src/main/java/hyung/jin/seo/jae/service/impl/AttendanceServiceImpl.java
+++ src/main/java/hyung/jin/seo/jae/service/impl/AttendanceServiceImpl.java
@@ -11,26 +11,28 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import hyung.jin.seo.jae.dto.AttendanceDTO;
+import hyung.jin.seo.jae.dto.mobile.AttendanceRollClazzDTO;
 import hyung.jin.seo.jae.model.Attendance;
+import hyung.jin.seo.jae.model.Clazz;
 import hyung.jin.seo.jae.repository.AttendanceRepository;
 import hyung.jin.seo.jae.service.AttendanceService;
 
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
-	
+
 	@Autowired
 	private AttendanceRepository attendanceRepository;
 
 	@Override
 	public List<AttendanceDTO> allAttendances() {
 		List<Attendance> attends = new ArrayList<>();
-		try{
+		try {
 			attends = attendanceRepository.findAll();
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		List<AttendanceDTO> dtos = new ArrayList<>();
-		for(Attendance attend: attends){
+		for (Attendance attend : attends) {
 			AttendanceDTO dto = new AttendanceDTO(attend);
 			dtos.add(dto);
 		}
@@ -40,33 +42,33 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public List<AttendanceDTO> findAttendanceByStudent(Long studentId) {
 		List<AttendanceDTO> dtos = new ArrayList<>();
-		try{
+		try {
 			dtos = attendanceRepository.findAttendanceByStudentId(studentId);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
-		}	
+		}
 		return dtos;
 	}
 
 	@Override
 	public List<AttendanceDTO> findAttendanceByClazz(Long claszzId) {
 		List<AttendanceDTO> dtos = new ArrayList<>();
-		try{
+		try {
 			dtos = attendanceRepository.findAttendanceByClazzId(claszzId);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
-		}	
+		}
 		return dtos;
 	}
 
 	@Override
 	public List<AttendanceDTO> findAttendanceByStudentAndClazz(Long studentId, Long clazzId) {
 		List<AttendanceDTO> dtos = new ArrayList<>();
-		try{
+		try {
 			dtos = attendanceRepository.findAttendanceByStudentIdAndClazzId(studentId, clazzId);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
-		}	
+		}
 		return dtos;
 	}
 
@@ -80,18 +82,22 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public Attendance updateAttendance(Attendance attend, Long id) {
 		// search by getId
-		Attendance existing = attendanceRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Attendance not found"));
+		Attendance existing = attendanceRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Attendance not found"));
 		// Update info
 		// status
-		if(!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getStatus()), StringUtils.defaultString(existing.getStatus()))){
+		if (!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getStatus()),
+				StringUtils.defaultString(existing.getStatus()))) {
 			existing.setStatus(StringUtils.defaultString(attend.getStatus()));
 		}
 		// week
-		if(!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getWeek()), StringUtils.defaultString(existing.getWeek()))){
+		if (!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getWeek()),
+				StringUtils.defaultString(existing.getWeek()))) {
 			existing.setWeek(StringUtils.defaultString(attend.getWeek()));
 		}
 		// info
-		if(!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getInfo()), StringUtils.defaultString(existing.getInfo()))){
+		if (!StringUtils.equalsIgnoreCase(StringUtils.defaultString(attend.getInfo()),
+				StringUtils.defaultString(existing.getInfo()))) {
 			existing.setInfo(StringUtils.defaultString(attend.getInfo()));
 		}
 		// update the existing record
@@ -102,9 +108,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public List<Long> findAttendanceIdByStudent(Long studentId) {
 		List<Long> attendIds = new ArrayList<>();
-		try{
-			attendIds = attendanceRepository.findAttendanceIdByStudentId(studentId);	
-		}catch(Exception e){
+		try {
+			attendIds = attendanceRepository.findAttendanceIdByStudentId(studentId);
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return attendIds;
@@ -113,9 +119,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public List<Long> findAttendanceIdByClazz(Long clazzId) {
 		List<Long> attendIds = new ArrayList<>();
-		try{
-			attendIds = attendanceRepository.findAttendanceIdByClazzId(clazzId);	
-		}catch(Exception e){
+		try {
+			attendIds = attendanceRepository.findAttendanceIdByClazzId(clazzId);
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return attendIds;
@@ -124,9 +130,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public List<Long> findAttendanceIdByStudentAndClazz(Long studentId, long clazzId) {
 		List<Long> attendIds = new ArrayList<>();
-		try{
-			attendIds = attendanceRepository.findAttendanceIdByStudentIdAndClazzId(studentId, clazzId);	
-		}catch(Exception e){
+		try {
+			attendIds = attendanceRepository.findAttendanceIdByStudentIdAndClazzId(studentId, clazzId);
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return attendIds;
@@ -135,9 +141,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public Attendance getAttendance(Long id) {
 		Attendance attend = null;
-		try{
+		try {
 			attend = attendanceRepository.findById(id).get();
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return attend;
@@ -151,9 +157,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 
 	@Override
 	public void deleteAttendance(Long id) {
-		try{
+		try {
 			attendanceRepository.deleteById(id);
-		}catch(EmptyResultDataAccessException e){
+		} catch (EmptyResultDataAccessException e) {
 			System.out.println("No attendance to delete");
 		}
 	}
@@ -161,9 +167,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public List<Long> findStudentIdByClazz(Long clazzId) {
 		List<Long> studentIds = new ArrayList<>();
-		try{
-			studentIds = attendanceRepository.findStudentIdByClazzId(clazzId);	
-		}catch(Exception e){
+		try {
+			studentIds = attendanceRepository.findStudentIdByClazzId(clazzId);
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return studentIds;
@@ -172,9 +178,9 @@ public class AttendanceServiceImpl implements AttendanceService {
 	@Override
 	public Attendance getAttendanceByStudentAndClazzAndWeek(Long studentId, Long clazzId, int week) {
 		Attendance attend = null;
-		try{
-			attend = attendanceRepository.getAttendanceByStudentIdAndClazzIdAndWeek(studentId, clazzId, week+"");
-		}catch(Exception e){
+		try {
+			attend = attendanceRepository.getAttendanceByStudentIdAndClazzIdAndWeek(studentId, clazzId, week + "");
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 		return attend;
@@ -182,20 +188,20 @@ public class AttendanceServiceImpl implements AttendanceService {
 
 	@Override
 	public void updateStatus(Long studentId, Long clazzId, int week, String status) {
-		try{
-			attendanceRepository.updateStatusByStudentIdAndClazzIdAndWeek(studentId, clazzId, week+"", status);
-		}catch(Exception e){
+		try {
+			attendanceRepository.updateStatusByStudentIdAndClazzIdAndWeek(studentId, clazzId, week + "", status);
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 	}
 
 	@Override
 	public void updateDay(Long id, String day) {
-		try{
+		try {
 			attendanceRepository.updateDay(id, day);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("No attendance found");
 		}
 	}
-	
+
 }
