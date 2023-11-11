d50a46be1de38b43a1ed5f38a294817a6f6a82e2
Jin <jin@macbookk2023.com>
2023-11-11 15:34:47 +1100

    retrieve student list from clazz

--- src/main/java/hyung/jin/seo/jae/SqlScriptRunner.java
+++ src/main/java/hyung/jin/seo/jae/SqlScriptRunner.java
@@ -14,29 +14,31 @@ import java.sql.SQLException;
 @Component
 public class SqlScriptRunner {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
+	@Autowired
+	private ResourceLoader resourceLoader;
 
-    @Autowired
-    private DataSource dataSource;
-    
-    @Autowired
-    private Environment env;
+	@Autowired
+	private DataSource dataSource;
 
-    @PostConstruct
-    public void runSqlScripts() throws SQLException {
-    	String ddl = env.getProperty("spring.jpa.hibernate.ddl-auto");
-    	// run scripts only for 'create-drop' or 'create'
-    	if(StringUtils.startsWithIgnoreCase(ddl, "create")){
-	        Connection connection = dataSource.getConnection();
-	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/elearning.sql")); // eLearning 
-	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/course.sql")); // Course
+	@Autowired
+	private Environment env;
+
+	@PostConstruct
+	public void runSqlScripts() throws SQLException {
+		String ddl = env.getProperty("spring.jpa.hibernate.ddl-auto");
+		// run scripts only for 'create-drop' or 'create'
+		if (StringUtils.startsWithIgnoreCase(ddl, "create")) {
+			Connection connection = dataSource.getConnection();
+			// ScriptUtils.executeSqlScript(connection,
+			// resourceLoader.getResource("classpath:sql/elearning.sql")); // eLearning
+			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/course.sql")); // Course
 			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/subject.sql")); // Subject
 			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/cycle.sql")); // Cycle
-	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/course_subject.sql")); // Course_Subject
-	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/book.sql")); // Course Book
-	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/class.sql")); // Class
-	    	ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/braybrook_student.sql")); // Student
-    	}
-    }
+			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/course_subject.sql")); // Course_Subject
+			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/book.sql")); // Course
+																											// Book
+			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/class.sql")); // Class
+			ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/braybrook_student.sql")); // Student
+		}
+	}
 }
--- src/main/java/hyung/jin/seo/jae/controller/rest/JaeRestController.java
+++ src/main/java/hyung/jin/seo/jae/controller/rest/JaeRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import hyung.jin.seo.jae.dto.AttendanceDTO;
 import hyung.jin.seo.jae.dto.ClazzDTO;
 import hyung.jin.seo.jae.dto.mobile.AttendanceRollClazzDTO;
 import hyung.jin.seo.jae.dto.mobile.AttendanceRollStudentDTO;
@@ -45,6 +46,9 @@ public class JaeRestController {
 	@Autowired
 	private CycleService cycleService;
 
+	@Autowired
+	private AttendanceService attendanceService;
+
 	@GetMapping("/clazzList/{id}")
 	List<AttendanceRollClazzDTO> getClazzList(@PathVariable Long id) {
 		List<AttendanceRollClazzDTO> dtos = new ArrayList<>();
@@ -69,63 +73,24 @@ public class JaeRestController {
 		return dtos;
 	}
 
-	@GetMapping("/attendList/{id}/{date}")
-	List<AttendanceRollStudentDTO> getAttendList(@PathVariable Long id, @PathVariable String date) {
+	@GetMapping("/attendList/{id}")
+	List<AttendanceRollStudentDTO> getAttendList(@PathVariable Long id) {
 		List<AttendanceRollStudentDTO> dtos = new ArrayList<>();
-
-		AttendanceRollStudentDTO dto1 = new AttendanceRollStudentDTO(1, "2020-01-01", "Y", 101321, "John Malcom");
-		AttendanceRollStudentDTO dto2 = new AttendanceRollStudentDTO(2, "2020-01-01", "Y", 102112, "Mary Go-round");
-		AttendanceRollStudentDTO dto3 = new AttendanceRollStudentDTO(3, "2020-01-01", "Y", 103134, "Jane Ngune");
-		AttendanceRollStudentDTO dto4 = new AttendanceRollStudentDTO(4, "2020-01-01", "N", 104343, "Peter Williams");
-		AttendanceRollStudentDTO dto5 = new AttendanceRollStudentDTO(5, "2020-01-01", "Y", 105643, "Paul Smith");
-		AttendanceRollStudentDTO dto6 = new AttendanceRollStudentDTO(6, "2020-01-01", "Y", 106334, "James Bond");
-		AttendanceRollStudentDTO dto7 = new AttendanceRollStudentDTO(7, "2020-01-01", "Y", 107532, "Simon Peter");
-		AttendanceRollStudentDTO dto8 = new AttendanceRollStudentDTO(8, "2020-01-01", "Y", 108432, "Judas Iscariot");
-		AttendanceRollStudentDTO dto9 = new AttendanceRollStudentDTO(9, "2020-01-01", "N", 109674, "Thomas Didymus");
-		AttendanceRollStudentDTO dto10 = new AttendanceRollStudentDTO(10, "2020-01-01", "N", 110332,
-				"Andrew the Apostle");
-		AttendanceRollStudentDTO dto11 = new AttendanceRollStudentDTO(11, "2020-01-01", "Y", 111274,
-				"Bartholomew the Apostle");
-		AttendanceRollStudentDTO dto12 = new AttendanceRollStudentDTO(12, "2020-01-01", "Y", 112212,
-				"Matthew the Apostle");
-		AttendanceRollStudentDTO dto13 = new AttendanceRollStudentDTO(13, "2020-01-01", "Y", 113480,
-				"Philip the Apostle");
-		AttendanceRollStudentDTO dto14 = new AttendanceRollStudentDTO(14, "2020-01-01", "Y", 114093, "James the Less");
-
-		dtos.add(dto1);
-		dtos.add(dto2);
-		dtos.add(dto3);
-		dtos.add(dto4);
-		dtos.add(dto5);
-		dtos.add(dto6);
-		dtos.add(dto7);
-		dtos.add(dto8);
-		dtos.add(dto9);
-		dtos.add(dto10);
-		dtos.add(dto11);
-		dtos.add(dto12);
-		dtos.add(dto13);
-		dtos.add(dto14);
+		// 1. check currentWeek
+		int currentWeek = cycleService.academicWeeks();
+		// 2. get attend list
+		List<AttendanceDTO> attendList = attendanceService.findAttendanceByClazzAndWeek(id, currentWeek);
+		// 3. populate AttendanceRollStudentDTO
+		for (AttendanceDTO attend : attendList) {
+			AttendanceRollStudentDTO dto = new AttendanceRollStudentDTO();
+			dto.setId(attend.getId());
+			dto.setStatus(attend.getStatus());
+			dto.setStudentId(attend.getStudentId());
+			dto.setStudentName(attend.getStudentName());
+			dtos.add(dto);
+		}
+		// 4. return dtos
 		return dtos;
 	}
-
-	// @GetMapping("/student/{id}")
-	// Student getAttendList(@PathVariable Long id) {
-	// Student std = studentService.getStudent(id);
-	// return std;
-	// }
-
-	// @PutMapping("/student")
-	// Student updateStudentAttend(@RequestBody Student std) {
-	// Student add = studentService.addStudent(std);
-	// return add;
-	// }
-
-	// @PutMapping("/student/{id}")
-	// Student updateTeacher(@RequestBody Student newStudent, @PathVariable Long id)
-	// {
-	// Student updated = studentService.updateStudent(newStudent, id);
-	// return updated;
-	// }
 
 }
