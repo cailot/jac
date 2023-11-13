01f90542b2b69bc1e6f17d9ddc6512b05bd40e22
Jin <jin@macbookk2023.com>
2023-11-14 00:46:10 +1100

    mobile update done

--- src/main/java/hyung/jin/seo/jae/service/AttendanceService.java
+++ src/main/java/hyung/jin/seo/jae/service/AttendanceService.java
@@ -26,7 +26,7 @@ public interface AttendanceService {
 	Attendance updateAttendance(Attendance attendance, Long id);
 
 	// update status
-	void updateStatus(Long id, String status);
+	void updateStatus(String id, String status);
 
 	// update status
 	void updateStatus(Long studentId, Long clazzId, int week, String status);
