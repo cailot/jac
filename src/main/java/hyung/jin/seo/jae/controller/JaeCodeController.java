f87dcc76ca9e90a89160ebfd46c3c2e072ebb23e
Jin <jin@macbookk2023.com>
2023-11-25 02:01:42 +1100

    Branch CRUD ....

--- src/main/java/hyung/jin/seo/jae/controller/JaeCodeController.java
+++ src/main/java/hyung/jin/seo/jae/controller/JaeCodeController.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hyung.jin.seo.jae.dto.BranchDTO;
+import hyung.jin.seo.jae.dto.CourseDTO;
 import hyung.jin.seo.jae.dto.SimpleBasketDTO;
+import hyung.jin.seo.jae.model.Branch;
+import hyung.jin.seo.jae.model.Course;
+import hyung.jin.seo.jae.model.State;
 import hyung.jin.seo.jae.service.CodeService;
 import hyung.jin.seo.jae.utils.JaeConstants;
 
@@ -52,6 +61,41 @@ public class JaeCodeController {
 		}
 		model.addAttribute(JaeConstants.BRANCH_LIST, dtos);
 		return "branchPage";
+	}
+
+	// register new cycle
+	@PostMapping("/registerBranch")
+	@ResponseBody
+	public ResponseEntity<String> registerBranch(@RequestBody BranchDTO formData) {
+		try {
+			// 1. create Cycle
+			Branch branch = formData.convertToBranch(formData);
+			// 2. get State
+			State state = codeService.getState(Long.parseLong(formData.getStateId()));
+			if(state!=null){
+				state.addBranch(branch);
+				// 3. update state
+				codeService.updateState(state, state.getId());
+			}
+			// 3. return success;
+			return ResponseEntity.ok("\"Branch register success\"");
+		} catch (Exception e) {
+			String message = "Error registering Branch: " + e.getMessage();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+		}
+	}
+
+	// get branch by Id
+	@GetMapping("/getBranch/{id}")
+	@ResponseBody
+	public BranchDTO getBranch(@PathVariable("id") Long id) {
+		BranchDTO dto = new BranchDTO();
+		try{
+			dto = codeService.getBranch(id);
+		}catch(Exception e){
+			System.out.println("No branch found");
+		}
+		return dto;
 	}
 
 }
--- src/main/java/hyung/jin/seo/jae/repository/BranchRepository.java
+++ src/main/java/hyung/jin/seo/jae/repository/BranchRepository.java
@@ -5,23 +5,24 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import hyung.jin.seo.jae.dto.BranchDTO;
 import hyung.jin.seo.jae.model.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch, Long>{  
 	
 	List<Branch> findAll();
 
-	// @Query("SELECT new hyung.jin.seo.jae.dto.BranchDTO(b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.stateId) FROM Branch b")   	
-	@Query(value = "SELECT b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.stateId FROM Branch b ORDER BY b.stateId DESC", nativeQuery = true)
-	List<Object[]> getAllBranches();
+	@Query("SELECT new hyung.jin.seo.jae.dto.BranchDTO(b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.state.id) FROM Branch b ORDER BY b.code ASC")
+	List<BranchDTO> getAllBranches();
 
 	@Query(value = "SELECT b.name, b.code FROM Branch b", nativeQuery = true)   
 	List<Object[]> loadBranch();
 	
-	@Query(value = "SELECT b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.stateId FROM Branch b WHERE b.stateId = ?1 ORDER BY b.id DESC", nativeQuery = true)
-	List<Object[]> searchBranchByState(String state);
+	@Query("SELECT new hyung.jin.seo.jae.dto.BranchDTO(b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.state.id) FROM Branch b WHERE b.state.id = ?1 ORDER BY b.code ASC")
+	List<BranchDTO> searchBranchByState(long state);
 
-	// long count();
+	@Query("SELECT new hyung.jin.seo.jae.dto.BranchDTO(b.id, b.code, b.name, b.phone, b.email, b.address, b.abn, b.bank, b.bsb, b.accountNumber, b.accountName, b.info, b.state.id) FROM Branch b WHERE b.id = ?1")
+	BranchDTO findBranch(long id);
 
-	
+
 }
