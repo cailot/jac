a2d2afbf8c8453672158c071cf59810166d4f075
Jin <jin@macbookk2023.com>
2024-02-07 03:33:23 +1100

    batch update basic function...

--- src/main/java/hyung/jin/seo/jae/controller/StudentController.java
+++ src/main/java/hyung/jin/seo/jae/controller/StudentController.java
@@ -3,6 +3,7 @@ package hyung.jin.seo.jae.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -119,5 +120,13 @@ public class StudentController {
 		return "studentGradePage";
 	}
 
+	@PutMapping("/updateGrade/{listTo}")
+	@ResponseBody
+	public ResponseEntity<String> updateGrade(@PathVariable("listTo") String listTo, @RequestBody List<Long> ids) {
+		String grade = listTo;
+		studentService.batchUpdateGrade(ids, grade);
+		return ResponseEntity.ok("\"Grade Updates Successfully\"");
+	}
+	
 
 }
