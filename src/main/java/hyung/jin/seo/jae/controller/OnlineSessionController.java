2124532f2108b367b56d0ffce794ca902bbb7c9e
Jin <jin@macbookk2023.com>
2024-02-20 01:46:15 +1100

    online session list done

--- src/main/java/hyung/jin/seo/jae/controller/OnlineSessionController.java
+++ src/main/java/hyung/jin/seo/jae/controller/OnlineSessionController.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,19 +21,26 @@ import hyung.jin.seo.jae.dto.BookDTO;
 import hyung.jin.seo.jae.dto.ClazzDTO;
 import hyung.jin.seo.jae.dto.OnlineSessionDTO;
 import hyung.jin.seo.jae.model.Book;
+import hyung.jin.seo.jae.model.Clazz;
+import hyung.jin.seo.jae.model.Course;
+import hyung.jin.seo.jae.model.Cycle;
 import hyung.jin.seo.jae.model.OnlineSession;
 import hyung.jin.seo.jae.service.BookService;
+import hyung.jin.seo.jae.service.ClazzService;
 import hyung.jin.seo.jae.service.OnlineSessionService;
 import hyung.jin.seo.jae.utils.JaeConstants;
 
 @Controller
-@RequestMapping("onlineSesion")
+@RequestMapping("onlineSession")
 public class OnlineSessionController {
 
 	//private static final Logger LOG = LoggerFactory.getLogger(JaeBookController.class);
 
 	@Autowired
 	private OnlineSessionService onlineSessionService;
+
+	@Autowired
+	private ClazzService clazzService;
 
 	// search all online sessions
 	@GetMapping("/all")
@@ -63,14 +74,49 @@ public class OnlineSessionController {
 			@RequestParam(value = "listYear", required = false) String year, 
 			Model model) {
 		List<OnlineSessionDTO> dtos = new ArrayList();
+		String filteredGrade = StringUtils.defaultString(grade);
 		String filteredYear = StringUtils.defaultString(year, "0");
-		if(JaeConstants.ALL.equalsIgnoreCase(filteredYear)){
+	    if(JaeConstants.ALL.equalsIgnoreCase(filteredGrade)){
+			if(JaeConstants.ALL.equalsIgnoreCase(filteredYear)){ // grade & year = all
+				dtos = onlineSessionService.allOnlineSessions(); 
+			}else{ // grade = all, year = some
+				dtos = onlineSessionService.filterOnlineSessionByYear(Integer.parseInt(filteredYear)); 
+			}	
+		}else if(JaeConstants.ALL.equalsIgnoreCase(filteredYear)){ // grade = some, year = all
 			dtos = onlineSessionService.filterOnlineSessionByGrade(grade);
-		}else{
+		}else{// grade = some, year = some
 			dtos = onlineSessionService.filterOnlineSessionByGradeNYear(grade, Integer.parseInt(filteredYear));
 		}
 		model.addAttribute(JaeConstants.ONLINE_LIST, dtos);
 		return "onlineListPage";
+	}
+
+
+	// register online session
+	@PostMapping("/register")
+	@ResponseBody
+	public ResponseEntity<String> registerOnline(@RequestBody OnlineSessionDTO formData) {
+		try {
+			// 1. create OnlineSession
+			OnlineSession session = formData.convertToOnlineSession();
+			// 2. set active to true as default
+			session.setActive(true);
+			// 3. get Clazz
+			Clazz clazz = clazzService.getOnlineByGradeNYear(formData.getGrade(), formData.getYear());
+			if(clazz==null){
+				return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("\"Please register online class first\"");
+			}else{
+			// 4. assign Clazz
+			session.setClazz(clazz);
+			// 5. add OnlineSession
+			onlineSessionService.addOnlineSession(session);
+			// 6. return success;
+			return ResponseEntity.ok("\"Online Session register success\"");
+			}
+		} catch (Exception e) {
+			String message = "Error registering Online Session: " + e.getMessage();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+		}
 	}
 
 }
