b380a2b4fcbb5479f80a3aa9e4ba08a0feba458d
Jin <jin@macbookk2023.com>
2024-03-08 10:39:20 +1100

    ...

---
+++ src/main/java/hyung/jin/seo/jae/model/Practice.java
@@ -0,0 +1,59 @@
+package hyung.jin.seo.jae.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Practice")
+public class Practice {
+    
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Long id;
+
+    // @Column(length = 300, nullable = true)
+    // private String videoPath;
+
+    @Column(length = 300, nullable = true)
+    private String pdfPath;
+
+    @Column(length = 2, nullable = true)
+    private Integer volume;
+
+    @Column(length = 4, nullable = true)
+    private Integer year;
+
+    @Column(length = 50, nullable = true)
+    private String info;
+
+    @Column
+    private boolean active;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gradeId")
+    private Grade grade;
+	
+    @CreationTimestamp
+    private LocalDate registerDate;
+    
+}
