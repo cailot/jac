ae025db59a8da421b12c6d448bf39d3c394562fe
Jin <jin@macbookk2023.com>
2024-03-08 10:39:20 +1100

    ...

---
+++ target/classes/sql/practiceType.sql
@@ -0,0 +1,15 @@
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Mega English', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Mega Mathemeatics', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Mega General Ability', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('NAPLAN', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Revision English', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Revision Mathemeatics', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Revision Science', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Reeading Comprehension (EDU)', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Verbal Reasoning (EDU)', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Mathematics (EDU)', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Numerical Reasoning (EDU)', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Humanities (ACER)', sysdate());
+INSERT INTO jae.PracticeType(name, registerDate) VALUES ('Mathematics (ACER)', sysdate());
+
+commit;
