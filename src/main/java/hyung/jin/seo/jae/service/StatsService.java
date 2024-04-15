5382a62e50a5413fb0fa3d9c3e98e8132d98f086
Jin <jin@macbookk2023.com>
2024-04-16 01:07:35 +1000

    bring student list at stats

--- src/main/java/hyung/jin/seo/jae/service/StatsService.java
+++ src/main/java/hyung/jin/seo/jae/service/StatsService.java
@@ -3,9 +3,17 @@ package hyung.jin.seo.jae.service;
 import java.util.List;
 
 import hyung.jin.seo.jae.dto.StatsDTO;
+import hyung.jin.seo.jae.dto.StudentDTO;
 
 public interface StatsService {
 
-	// search registration stats
-	List<StatsDTO> getRegistrationStats(String from, String to);	
+	// search active student stats
+	List<StatsDTO> getActiveStats(String from, String to);
+
+	// search inactive student stats
+	List<StatsDTO> getInactiveStats(String from, String to);
+
+	// list students by branch, grade, date & active
+	List<StudentDTO> listStudent4Stats(String branch, String grade, String from, String to, int active);
+
 }
--- src/main/java/hyung/jin/seo/jae/service/impl/StatsServiceImpl.java
+++ src/main/java/hyung/jin/seo/jae/service/impl/StatsServiceImpl.java
@@ -1,5 +1,7 @@
 package hyung.jin.seo.jae.service.impl;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hyung.jin.seo.jae.dto.StatsDTO;
+import hyung.jin.seo.jae.dto.StudentDTO;
 import hyung.jin.seo.jae.repository.StudentRepository;
 import hyung.jin.seo.jae.service.StatsService;
 
@@ -17,13 +20,39 @@ public class StatsServiceImpl implements StatsService {
 	StudentRepository studentRepository;
 
 	@Override
-	public List<StatsDTO> getRegistrationStats(String from, String to) {
-		List<Object[]> objects = studentRepository.getRegistrationStats(from, to);
+	public List<StatsDTO> getActiveStats(String from, String to) {
+		List<Object[]> objects = studentRepository.getActiveStudentStats(from, to);
 		// convert result to StatsDTO
 		List<StatsDTO> dtos = new ArrayList<>();
 		for(Object[] object : objects){
 			StatsDTO dto = new StatsDTO(object);
 			dtos.add(dto);
+		}
+		return dtos;
+	}
+
+	@Override
+	public List<StatsDTO> getInactiveStats(String from, String to) {
+		List<Object[]> objects = studentRepository.getInactiveStudentStats(from, to);
+		// convert result to StatsDTO
+		List<StatsDTO> dtos = new ArrayList<>();
+		for(Object[] object : objects){
+			StatsDTO dto = new StatsDTO(object);
+			dtos.add(dto);
+		}
+		return dtos;
+	}
+
+	@Override
+	public List<StudentDTO> listStudent4Stats(String branch, String grade, String from, String to, int active) {
+		List<StudentDTO> dtos = new ArrayList<>();
+		try{
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			LocalDate fromDate = LocalDate.parse(from, formatter);
+			LocalDate toDate = LocalDate.parse(to, formatter);
+			dtos = studentRepository.listStudent4Stats(branch, grade, fromDate, toDate, active);
+		}catch(Exception e){
+			System.out.println("No Student found");
 		}
 		return dtos;
 	}
