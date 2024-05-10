05a4a1544c8a83d5e7220a6aa109cccb9cc877bf
Jin <jin@macbookk2023.com>
2024-05-10 17:16:25 +1000

    javamail with html format & attachment done

---
+++ src/main/java/hyung/jin/seo/jae/service/EmailService.java
@@ -0,0 +1,10 @@
+package hyung.jin.seo.jae.service;
+
+public interface EmailService {
+
+	// send simple email
+	void sendEmail(String from, String to, String subject, String body);
+
+	// send simple email with attachment
+	void sendEmailWithAttachment(String from, String to, String subject, String body);
+}
---
+++ src/main/java/hyung/jin/seo/jae/service/impl/EmailServiceImpl.java
@@ -0,0 +1,73 @@
+package hyung.jin.seo.jae.service.impl;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import hyung.jin.seo.jae.service.EmailService;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+	
+	@Autowired
+	JavaMailSender mailSender;
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Override
+	public void sendEmail(String from, String to, String subject, String body) {
+		MimeMessage message = mailSender.createMimeMessage();
+		try {
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(MimeMessage.RecipientType.TO, to);
+			message.setSubject(subject);
+			String contents =  "<h1>This is a test Spring Boot email</h1>" +
+			"<marquee><p>It can contain <strong>HTML</strong> content.</p></marquee>";
+			message.setContent(contents, "text/html; charset=utf-8");
+			mailSender.send(message);
+			System.out.println("MAIL SENT SUCCESSFULLY");
+	
+
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void sendEmailWithAttachment(String from, String to, String subject, String body) {
+		MimeMessage message = mailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setTo(to);
+			helper.setSubject(subject);
+			String contents =  "<h1>This is a test Spring Boot email</h1>" +
+			"<marquee><p>It can contain <strong>HTML</strong> content.</p></marquee>";
+			helper.setText(contents);
+
+			Resource resource = resourceLoader.getResource("classpath:pdf/a.pdf");
+			FileSystemResource file = new FileSystemResource(resource.getFile());
+			helper.addAttachment("invoice.pdf", file);
+			mailSender.send(message);
+			System.out.println("HTML WITH ATTACHMENT MAIL SENT SUCCESSFULLY");
+	
+
+		} catch (MessagingException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
