e9895a1f1596e295cd6dc92d17898882d5b157dc
Jin <jin@macbookk2023.com>
2024-05-14 22:32:11 +1000

    pdf converstion....

--- src/main/java/hyung/jin/seo/jae/service/impl/EmailServiceImpl.java
+++ src/main/java/hyung/jin/seo/jae/service/impl/EmailServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
