97bbee3d08bb9b771cc4a8df12fbf3e033f36d0c
Jin <jin@macbookk2023.com>
2024-05-29 17:58:21 +1000

    DBConfig add

---
+++ src/main/java/hyung/jin/seo/jae/config/JaeDBConfig.java
@@ -0,0 +1,42 @@
+package hyung.jin.seo.jae.config;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class JaeDBConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    
+    @Bean
+    public DataSource datasource() {
+      return DataSourceBuilder.create()
+      .driverClassName(driverClassName)
+      .url(url)
+      .username(username)
+      .password(password)
+      .build(); 
+    }
+
+}
