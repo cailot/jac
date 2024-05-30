687617af24f16ce97bdcd78c1cc7322721999f06
Jin <jin@macbookk2023.com>
2024-05-30 23:35:40 +1000

    upload application.properties

---
+++ target/jac-management-1.0.0/BOOT-INF/classes/sql/cycle.sql
@@ -0,0 +1,10 @@
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2019,'2019/20 Academic Calendar', '2019-06-16', '2020-06-13', '2019-12-23', '2020-01-10', CURDATE());
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2020,'2020/21 Academic Calendar', '2020-06-14', '2021-06-12', '2020-12-21', '2021-01-08', CURDATE());
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2021,'2021/22 Academic Calendar', '2021-06-13', '2022-06-11', '2021-12-20', '2022-01-07', CURDATE());
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2022,'2022/23 Academic Calendar', '2022-06-12', '2023-06-10', '2022-12-19', '2023-01-06', CURDATE());
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2023,'2023/24 Academic Calendar', '2023-06-11', '2024-06-08', '2023-12-25', '2024-01-13', CURDATE());
+INSERT INTO jac.Cycle (year, description, startDate, endDate, vacationStartDate, vacationEndDate, registerDate) VALUES (2024,'2024/25 Academic Calendar', '2024-06-09', '2025-06-07', '2024-12-23', '2025-01-11', CURDATE());
+
+COMMIT;
+
+
