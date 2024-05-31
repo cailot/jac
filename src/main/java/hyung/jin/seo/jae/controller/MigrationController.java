9da7c0cc8b025c81708504058d1b0b10dee7c7f2
Jin <jin@macbookk2023.com>
2024-05-31 16:19:54 +1000

    ...

--- src/main/java/hyung/jin/seo/jae/controller/MigrationController.java
+++ src/main/java/hyung/jin/seo/jae/controller/MigrationController.java
@@ -1,8 +1,17 @@
 package hyung.jin.seo.jae.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +24,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import hyung.jin.seo.jae.dto.CourseDTO;
 import hyung.jin.seo.jae.dto.StudentDTO;
 import hyung.jin.seo.jae.model.Student;
 import hyung.jin.seo.jae.service.StudentService;
@@ -33,6 +45,105 @@ public class MigrationController {
 	private StudentService studentService;
 
 	
+	// migrate student
+	@RequestMapping(value = "/upload", method = {RequestMethod.POST})
+	public String migrateStudents(@RequestParam(value = "file", required = false) MultipartFile file, Model model) {
+			// 1. validate uploaded file	
+			if (file != null && !file.isEmpty()) {
+	            String originalFilename = file.getOriginalFilename();
+	            if (originalFilename != null) {
+	                String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+	                if ("csv".equalsIgnoreCase(fileExtension)) {
+	                    // File extension is CSV, proceed with further processing
+	                } else {
+	                    // Invalid file extension
+	                    model.addAttribute(JaeConstants.ERROR, "Invalid file format. Please upload a CSV file.");
+	                    return "batchHpiiPage"; // Redirect to the batchHpii endpoint to display the error message
+	                }
+	            } else {
+	                // File name not found
+	                model.addAttribute(JaeConstants.ERROR, "File name not found. Please try again.");
+	                return "batchHpiiPage"; // Redirect to the batchHpii endpoint to display the error message
+	            }
+	        } else {
+	            // No file uploaded
+	            model.addAttribute(JaeConstants.ERROR, "No file uploaded. Please select a file to upload.");
+	            return "batchHpiiPage"; // Redirect to the batchHpii endpoint to display the error message
+	        }
+			
+			// 2. proccess student migration
+			List<StudentDTO> dtos = new ArrayList<StudentDTO>();
+			int lineCount = 0;
+            int hpiiCount = 0;
+            if (file != null && !file.isEmpty()) {
+	            try {
+	            	// Create a BufferedReader to read the lines from the uploaded CSV file
+	            	BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+	                String line;
+	                while ((line = reader.readLine()) != null) {
+	                    lineCount++;
+	                    String[] columns = line.split(",");
+	                    
+	                    // if (!((columns.length == 3) || (columns.length == 4))) {
+	                    //     // If a line doesn't have 3 columns, throw an error
+	                    //     model.addAttribute(JaeConstants.ERROR, "Invalid format on line " + lineCount);
+	                    //     dtos = null;
+	                    //     break;
+	                    // }
+	                    if(lineCount ==1) continue; // skip header in csv
+	                    String firstName = columns[0];
+						String lastName = columns[1];
+	                    String password = columns[2];
+	                    String grade = columns[3];
+						String contactNo1 = columns[4];
+	                    String contactNo2 = columns[5];
+	                    String email1 = columns[6];
+						String email2 = columns[7];
+	                    String relation1 = columns[8];
+	                    String relation2 = columns[9];
+						String address = columns[10];
+	                    String state = columns[11];
+	                    String branch = columns[12];
+						String memo = columns[13];
+	                    String gender = columns[12];
+	                    String registerDate = columns[15];
+	                    String endDate = columns[16];
+	                    // create Student
+	                    Student std =  new Student();
+						if(StringUtils.isNotBlank(firstName)) std.setFirstName(firstName);
+						if(StringUtils.isNotBlank(lastName)) std.setLastName(lastName);
+						BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+						String encodedPassword = passwordEncoder.encode(password);
+						std.setPassword(encodedPassword);	
+						if(StringUtils.isNotBlank(grade)) std.setGrade(grade);
+						if(StringUtils.isNotBlank(contactNo1)) std.setContactNo1(contactNo1);
+						if(StringUtils.isNotBlank(contactNo2)) std.setContactNo2(contactNo2);
+						if(StringUtils.isNotBlank(email1)) std.setEmail1(email1);
+						if(StringUtils.isNotBlank(email2)) std.setEmail2(email2);
+						if(StringUtils.isNotBlank(relation1)) std.setRelation1(relation1);
+						if(StringUtils.isNotBlank(relation2)) std.setRelation2(relation2);
+						if(StringUtils.isNotBlank(address)) std.setAddress(address);
+						if(StringUtils.isNotBlank(state)) std.setState(state);
+						if(StringUtils.isNotBlank(branch)) std.setBranch(branch);
+						if(StringUtils.isNotBlank(memo)) std.setMemo(memo);
+						if(StringUtils.isNotBlank(gender)) std.setGender(gender);
+						if(StringUtils.isNotBlank(registerDate)) std.setRegisterDate(LocalDate.parse(registerDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    					if(StringUtils.isNotBlank(endDate)) std.setEndDate(LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+						
+						// register Student	
+	                    std = studentService.addStudent(std);
+	                    dtos.add(new StudentDTO(std));
+	                }
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+			model.addAttribute(JaeConstants.SUCCESS, hpiiCount);
+			model.addAttribute(JaeConstants.TOTAL, (lineCount-1));// except 1st line header
+			model.addAttribute(JaeConstants.HPII_LIST, dtos);
+		
+		return "migrationPage";
+	}
 	// register new student
 	@PostMapping("/register")
 	@ResponseBody
