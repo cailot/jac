033568967cfc8a70c9da32ce1451a474c1b7314e
Jin <jin@macbookk2023.com>
2024-07-10 21:19:14 +1000

    upgrade.jsp selection fixed

--- src/main/java/hyung/jin/seo/jae/repository/StudentPracticeRepository.java
+++ src/main/java/hyung/jin/seo/jae/repository/StudentPracticeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,24 +20,25 @@ public interface StudentPracticeRepository extends JpaRepository<StudentPractice
 	@SuppressWarnings("null")
 	Optional<StudentPractice> findById(Long id);
 
-    @Query("SELECT new hyung.jin.seo.jae.dto.StudentPracticeDTO(" +
-            "sp.id, " +
-            "sp.registerDate, " +
-            "sp.score, " +
-            "sp.student.id, " +
-            "sp.practice.id, " +
-            "sp.answers) " +
-            "FROM StudentPractice sp " +
-            "WHERE sp.student.id = :studentId AND sp.practice.id = :practiceId")
-    StudentPracticeDTO findStudentPractice(
-            @Param("studentId") Long studentId,
-            @Param("practiceId") Long practiceId
-    );	// // bring latest EnrolmentDTO by student id, called from retrieveEnrolment() in
+        @Query("SELECT new hyung.jin.seo.jae.dto.StudentPracticeDTO(" +
+                "sp.id, " +
+                "sp.registerDate, " +
+                "sp.score, " +
+                "sp.student.id, " +
+                "sp.practice.id, " +
+                "sp.answers) " +
+                "FROM StudentPractice sp " +
+                "WHERE sp.student.id = :studentId AND sp.practice.id = :practiceId")
+        StudentPracticeDTO findStudentPractice(
+                @Param("studentId") Long studentId,
+                @Param("practiceId") Long practiceId
+        );	// // bring latest EnrolmentDTO by student id, called from retrieveEnrolment() in
 	// // courseInfo.jsp
-	// check whether there is a record in StudentPractice table by studentId and practiceId
-	Optional<StudentPractice> findByStudentIdAndPracticeId(Long studentId, Long practiceId);
-	
-	// delete existing record in StudentPracice table by studentId and practiceId
-	void deleteByStudentIdAndPracticeId(Long studentId, Long practiceId);
+        // check whether there is a record in StudentPractice table by studentId and practiceId
+        Optional<StudentPractice> findByStudentIdAndPracticeId(Long studentId, Long practiceId);
+
+        // delete existing record in StudentPracice table by studentId and practiceId
+        @Modifying
+        void deleteByStudentIdAndPracticeId(Long studentId, Long practiceId);
 
 }
