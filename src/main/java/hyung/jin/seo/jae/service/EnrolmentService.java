ec2a6bb69d4b276817aab95dce1c835eaf45f607
Jin <jin@macbookk2023.com>
2024-08-18 23:55:13 +1000

    완료페이먼트 필요??

--- src/main/java/hyung/jin/seo/jae/service/EnrolmentService.java
+++ src/main/java/hyung/jin/seo/jae/service/EnrolmentService.java
@@ -15,6 +15,9 @@ public interface EnrolmentService {
 	// list enrolments by invoice Id
 	List<EnrolmentDTO> findEnrolmentByInvoice(Long invoiceId);
 
+	// list raw enrolments by invoice Id
+	List<Enrolment> getEnrolmentByInvoice(Long invoiceId);	
+
 	// list enrolments by invoice history Id
 	List<EnrolmentDTO> findEnrolmentByInvoiceHistory(Long invoiceHistoryId);
 
