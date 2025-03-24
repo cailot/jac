dd6231e59187ae6f4a2ffbe8def6112c424f2b08
Jin <jin@macbookk2023.com>
2025-03-24 22:15:48 +1100

    OMR 집계표 완성

--- src/main/java/hyung/jin/seo/jae/dto/OmrStatsDTO.java
+++ src/main/java/hyung/jin/seo/jae/dto/OmrStatsDTO.java
@@ -1,6 +1,8 @@
 package hyung.jin.seo.jae.dto;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +25,13 @@ public class OmrStatsDTO implements Serializable{
     private int acer;
     
     private int edu;
+
+    public OmrStatsDTO(Object[] obj) {
+        this.branch = (obj[0]!=null) ? String.valueOf(obj[0]) : "0";
+		this.mega = (obj[1]!=null) ? Integer.parseInt(String.valueOf(obj[1])) : 0;
+        this.revision = (obj[2]!=null) ? Integer.parseInt(String.valueOf(obj[2])) : 0;
+        this.acer = (obj[3] != null) ? Integer.parseInt(String.valueOf(obj[3])) : 0;
+        this.edu = (obj[4] != null) ? Integer.parseInt(String.valueOf(obj[4])) : 0;
+    }
 
 }
